52e2d4a7b6c5badb7380c662c06933763fa321e2
Daniel Yoo <jarkassp2@gmail.com>
2019-02-19 03:16:56 -0500

    Initial Commit

---
+++ components/hoc/Meta.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const Meta = () => {
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+        type="text/css"
+        rel="stylesheet"
+      />
+
+      <title>Enter Title</title>
+    </Head>
+  );
+};
+
+export default Meta;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Icon } from "semantic-ui-react";
+import Link from "next/link";
+
+class Home extends Component {
+  state = {};
+
+  render() {
+    return <div>Hi</div>;
+  }
+}
+
+export default Home;
